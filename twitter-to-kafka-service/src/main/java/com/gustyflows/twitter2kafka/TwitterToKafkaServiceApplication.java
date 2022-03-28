68ee3835c29df3cc983da4385700a659914508b9
augustas3000 <augusto.pasaulis@gmail.com>
2022-03-28 20:55:14 +0300

    add basic setup for config server

--- twitter-to-kafka-service/src/main/java/com/gustyflows/twitter2kafka/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/gustyflows/twitter2kafka/TwitterToKafkaServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 
 @Slf4j
 @EnableConfigurationProperties
