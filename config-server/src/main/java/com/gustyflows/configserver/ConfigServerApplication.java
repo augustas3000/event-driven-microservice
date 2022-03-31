14e21e5a81e6e5f5351a72c9232989645e10933e
augustas3000 <augusto.pasaulis@gmail.com>
2022-03-31 09:19:58 +0300

    link config-server app to remote repo, add encrypted creds and configure jsypt decryption

--- config-server/src/main/java/com/gustyflows/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/gustyflows/configserver/ConfigServerApplication.java
@@ -1,10 +1,12 @@
 package com.gustyflows.configserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ConfigServerApplication {
     public static void main(String[] args) {
