de4361687728c6d6a7e4a8ed4edb3904ffe66890
augustas3000 <augusto.pasaulis@gmail.com>
2022-04-05 21:03:18 +0300

    start app, encryption, banner

--- kafka-to-elastic-service/src/main/java/com/gustyflows/kafka2elastic/KafkaToElasticServiceApplication.java
+++ kafka-to-elastic-service/src/main/java/com/gustyflows/kafka2elastic/KafkaToElasticServiceApplication.java
@@ -1,10 +1,20 @@
 package com.gustyflows.kafka2elastic;
 
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@Slf4j
+@EnableEncryptableProperties
+@EnableConfigurationProperties
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.gustyflows"
+        }
+)
 public class KafkaToElasticServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(KafkaToElasticServiceApplication.class, args);
