be52d83605c1c00c85df450422eb7e0788842071
augustas3000 <augusto.pasaulis@gmail.com>
2022-04-05 20:34:15 +0300

    add entry class

---
+++ kafka-to-elastic-service/src/main/java/com/gustyflows/kafka2elastic/KafkaToElasticServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gustyflows.kafka2elastic;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaToElasticServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
+    }
+}
