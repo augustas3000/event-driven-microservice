b1bf5dbb2b2c2e9c6610fd26a76f39d45977235a
augustas3000 <augusto.pasaulis@gmail.com>
2022-04-09 14:17:35 +0300

    add elastic search configuration

---
+++ elastic/elastic-config/src/main/java/com/gustyflows/elastic/config/ElasticSearchConfig.java
@@ -0,0 +1,39 @@
+package com.gustyflows.elastic.config;
+
+import com.gustyflows.config.twitter2kafka.ElasticConfigProperties;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Objects;
+
+@Configuration
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    private final ElasticConfigProperties elasticConfigProperties;
+
+    public ElasticSearchConfig(ElasticConfigProperties elasticConfigProperties) {
+        this.elasticConfigProperties = elasticConfigProperties;
+    }
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        UriComponents serverUri = UriComponentsBuilder.fromHttpUrl(elasticConfigProperties.getConnectionUrl()).build();
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost(
+                        Objects.requireNonNull(serverUri.getHost()),
+                        serverUri.getPort(),
+                        serverUri.getScheme()
+                )).setRequestConfigCallback(reqConfigBuilder ->
+                        reqConfigBuilder
+                                .setConnectTimeout(elasticConfigProperties.getConnectTimeoutMs())
+                                .setSocketTimeout(elasticConfigProperties.getSocketTimeoutMs()))
+        );
+    }
+}
