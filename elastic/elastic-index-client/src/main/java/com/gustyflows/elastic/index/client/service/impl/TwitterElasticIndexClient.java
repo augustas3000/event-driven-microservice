61f01c48cc7f17b49616b86cd8f6e6417a6e56bf
augustas3000 <augusto.pasaulis@gmail.com>
2022-04-11 21:19:45 +0300

    add elastic index client behaviour and impl for indexing TwitterIndexModel

---
+++ elastic/elastic-index-client/src/main/java/com/gustyflows/elastic/index/client/service/impl/TwitterElasticIndexClient.java
@@ -0,0 +1,51 @@
+package com.gustyflows.elastic.index.client.service.impl;
+
+import com.gustyflows.config.twitter2kafka.ElasticConfigProperties;
+import com.gustyflows.elastic.index.client.service.ElasticIndexClient;
+import com.gustyflows.elastic.index.client.util.ElasticIndexUtil;
+import com.gustyflows.elastic.model.index.impl.TwitterIndexModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexedObjectInformation;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class TwitterElasticIndexClient implements ElasticIndexClient<TwitterIndexModel> {
+
+    private final ElasticConfigProperties elasticConfigProperties;
+    private final ElasticsearchOperations elasticsearchOperations;
+    private final ElasticIndexUtil<TwitterIndexModel> elasticIndexUtil;
+
+    public TwitterElasticIndexClient(
+            ElasticConfigProperties elasticConfigProperties,
+            ElasticsearchOperations elasticsearchOperations,
+            ElasticIndexUtil<TwitterIndexModel> elasticIndexUtil
+    ) {
+        this.elasticConfigProperties = elasticConfigProperties;
+        this.elasticsearchOperations = elasticsearchOperations;
+        this.elasticIndexUtil = elasticIndexUtil;
+    }
+
+
+    @Override
+    public List<String> save(List<TwitterIndexModel> documents) {
+        List<IndexQuery> indexQueries = elasticIndexUtil.getIndexQueries(documents);
+
+        List<IndexedObjectInformation> indexedObjectInformations = elasticsearchOperations.bulkIndex(
+                indexQueries,
+                IndexCoordinates.of(elasticConfigProperties.getIndexName())
+        );
+
+        List<String> documentIds =
+                indexedObjectInformations.stream().map(IndexedObjectInformation::getId).collect(Collectors.toList());
+
+        log.info("Documents indexed successfully with type: {} and ids: {}", TwitterIndexModel.class.getName(), documentIds);
+        return documentIds;
+    }
+}
