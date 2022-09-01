f726777c98525da41205654ec01100c653163368
augustas3000 <augusto.pasaulis@gmail.com>
2022-09-01 09:41:43 +0300

    example of api versioning - url approach

--- elastic-query-service/src/main/java/com/gustyflows/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/gustyflows/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package com.gustyflows.elastic.query.service.api;
 import com.gustyflows.elastic.query.service.business.ElasticQueryService;
 import com.gustyflows.elastic.query.service.model.ElasticQueryServiceRequest;
 import com.gustyflows.elastic.query.service.model.ElasticQueryServiceResponse;
+import com.gustyflows.elastic.query.service.model.ElasticQueryServiceResponseV2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,19 +29,35 @@ public class ElasticDocumentController {
         this.elasticQueryService = elasticQueryService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/v1")
     public ResponseEntity<List<ElasticQueryServiceResponse>> getAllDocuments() {
         List<ElasticQueryServiceResponse> responses = elasticQueryService.getAllDocuments();
         return ResponseEntity.ok(responses);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/v1/{id}")
     public ResponseEntity<ElasticQueryServiceResponse> getDocumentById(@PathVariable @NotEmpty String id) {
         ElasticQueryServiceResponse response = elasticQueryService.getDocumentById(id);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/get-document-by-text")
+    @GetMapping("/v2/{id}")
+    public ResponseEntity<ElasticQueryServiceResponseV2> getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        ElasticQueryServiceResponse response = elasticQueryService.getDocumentById(id);
+        return ResponseEntity.ok(getV2Model(response));
+    }
+
+    private ElasticQueryServiceResponseV2 getV2Model(ElasticQueryServiceResponse response) {
+        return ElasticQueryServiceResponseV2.builder()
+                .id(Long.parseLong(response.getId()))
+                .text(response.getText())
+                .userId(response.getUserId())
+                .createdAt(response.getCreatedAt())
+                .build()
+                .add(response.getLinks());
+    }
+
+    @PostMapping("/v1/get-document-by-text")
     public ResponseEntity<List<ElasticQueryServiceResponse>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequest request) {
         List<ElasticQueryServiceResponse> responses = elasticQueryService.getDocumentByText(request.getText());
         return ResponseEntity.ok(responses);
