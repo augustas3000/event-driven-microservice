e48ac3ccb36a6b53671269243f15f67ab586d1bd
augustas3000 <augusto.pasaulis@gmail.com>
2022-09-12 08:41:06 +0300

    disable FeignHalAutoConfiguration.class as a quick fix for some version related error

--- elastic-query-service/src/main/java/com/gustyflows/elastic/query/service/ElasticQueryServiceApplication.java
+++ elastic-query-service/src/main/java/com/gustyflows/elastic/query/service/ElasticQueryServiceApplication.java
@@ -2,8 +2,9 @@ package com.gustyflows.elastic.query.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.hateoas.FeignHalAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.gustyflows")
+@SpringBootApplication(scanBasePackages = "com.gustyflows", exclude = {FeignHalAutoConfiguration.class})
 public class ElasticQueryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElasticQueryServiceApplication.class, args);
